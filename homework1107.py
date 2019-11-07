c62fe7c4bcbd3fcaaa05fe0555a54e3be01ae679
zxl <1210950941@qq.com>
2019-11-07 04:40:40 -0800

    update

--- homework1107.py
+++ homework1107.py
@@ -4,7 +4,6 @@ author：周小灵
 date：2019.11.06
 '''
 import pymysql
-import re
 from hashlib import sha1
 
 
@@ -20,7 +19,7 @@ try:
         cursor.execute("use bbs")
     else:
         print("创建失败")
-except Exception as e :
+except Exception as e:
     print(e)
     exit()
 
@@ -31,45 +30,58 @@ try:
            "username varchar(10) unique, " \
            "usertype enum('0','1') default '0'," \
            "password varchar(100), " \
-           "regitme time, " \
+           "regtime datetime default current_timestamp, " \
            "email varchar(50))"
     result = cursor.execute(sql1)
-    if result > 0:
-        print("创建成功")
-    else:
-        print("创建失败")
+    # if result > 0:
+    #     print("创建成功")
+    # else:
+    #     print("创建失败")
 except Exception as e:
     print(e)
-    exit()
+
+cursor.execute("use bbs")
 
 # 3.输入信息并验证，验证通过则写入user表
-username0=input("请输入用户名：")
-password0=input("请输入密码：")
+username0=input("请输入注册用户名：")
+password0=input("请输入注册密码：")
 password0=sha1(password0.encode('utf8')).hexdigest()
 email0=input("请输入注册邮箱：")
 
 sql2 = "select username from user"
 sql3 = "insert into user(username,password,email) values('{}','{}','{}')"
-if username0 in cursor.execute(sql2):
-    print('用户已经存在，请重新输入')
-else:
-    if len(username0) <2 or username0:
-        print('⽤户名⻓度必须⼤于2，不能为纯空格')
+cursor.execute(sql2)
+print(cursor.fetchall())
+try:
+    if (username0,) in cursor.fetchall():
+        print('用户已经存在，请重新输入')
+    elif len(username0) <2:
+        print('⽤户名⻓度必须⼤于2')
+    elif str.isspace(username0):
+        print('⽤户名不能为纯空格')
     else:
         sql3 = sql3.format(username0,password0,email0)
         cursor.execute(sql3)
+        conn.commit()
+except Exception as e:
+    print(e)
+
 
 # 4.用户登录
 username1=input("请输入用户名：")
 password1=input("请输入密码：")
 password1 = sha1(password1.encode('utf8')).hexdigest()
-sql4 = "select username,password from user where username='{}' and password='{}'".formate(username1,password1)
+sql4 = "select username,password from user where username='{}' and password='{}'".format(username1,password1)
 if cursor.execute(sql4):
     print('登录成功')
 else:
     print('用户名或密码错误，请重新输入')
 
+# 5.显示信息
 sql5 = "select username '用户名',usertype '用户类型', password '密码', regtime '注册时间', email from user"
 cursor.execute(sql5)
+print(cursor.fetchall())
 
+cursor.close()
+conn.close()
 
